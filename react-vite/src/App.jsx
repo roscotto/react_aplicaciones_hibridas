e6f7a4575735591ca4f7b4b04ed63d109ae589a2
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-05 10:31:01 -0300

    creacion de componentes con funciones trad y arrow

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,12 +1,43 @@
 //Componentes funcionales
+
+const Navbar = () => (
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Productos</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+)
+
+
+const Header = () => {
+  //código js
+  return(
+    <header>
+      <h1>Header</h1>
+      <Navbar />      
+    </header>
+  )
+}
+
+
+
 function App()
 {
   return(
     <div>
-      <h1>Hola mundo</h1>
+      <Header />
+      <h2>Hola mundo</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat tempora beatae modi, iste repellendus ad aliquid minus necessitatibus nesciunt accusamus esse harum est, veniam recusandae alias dolores magnam error!</p>
     </div>
   )
 }
 
 export default App;
+
+export {
+  App,
+  Header,
+  Navbar
+}
--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App, { Navbar } from './App.jsx'
+
 
 
 ReactDOM.createRoot(
   document.getElementById('root'))
     .render(
     //<React.StrictMode>
+    <>
+      <Navbar />
       <App />
+    </>
     //</React.StrictMode>,
 )
