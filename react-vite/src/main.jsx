c041a874c2595c8f9f238ba4f78fad4bcc8150ef
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-05 10:15:05 -0300

    componente separado en archivo

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-//Componentes funcionales
-function App()
-{
-  return(
-    <div>
-      <h1>Hola mundo</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat tempora beatae modi, iste repellendus ad aliquid minus necessitatibus nesciunt accusamus esse harum est, veniam recusandae alias dolores magnam error!</p>
-    </div>
-  )
-}
 
 ReactDOM.createRoot(
   document.getElementById('root'))
