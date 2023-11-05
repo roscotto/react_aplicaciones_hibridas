50fa0adbfe7806e1a7409fbc461c54b94aa0baa8
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-05 10:01:49 -0300

    creación primer componente

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//Componentes funcionales
+function App()
+{
+  return(
+    <div>
+      <h1>Hola mundo</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat tempora beatae modi, iste repellendus ad aliquid minus necessitatibus nesciunt accusamus esse harum est, veniam recusandae alias dolores magnam error!</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(
+  document.getElementById('root'))
+    .render(
+    //<React.StrictMode>
+      <App />
+    //</React.StrictMode>,
 )
