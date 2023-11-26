1d9b5d91f803637068d306ce86c6c394e42c5461
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-26 14:50:18 -0300

    estados + ciclo de vida

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -7,22 +7,28 @@ import './App.css';
 const Navbar = ({ dark, links }) => 
 {
   let className = "nav-principal";
-
   if(dark){
     className += " nav-principal--dark";
   }
-
   
   //forma estructurada
+  // const liLinks = [];
   // for(let i = 0; i < links?.length; i++){
-  //   liLinks.push(<li className="nav-principal__item"><a href={links[i].url}>{links[i].texto}</a></li>
+  //   liLinks.push(<li className="nav-principal__item">
+  //    <a href={links[i].url}>{links[i].texto}</a></li>
   //   )
   // }
+  
+  //forma funcional
+  // const liLinks = links.map((elemento) => <li className="nav-principal__item"> <a href={elemento.url}>{elemento.texto}</a></li>)
 
   return(
     <nav>
       <ul className={className}>
-        {links.map((elemento, index) => <li key={index} className="nav-principal__item"><a href={elemento.url}>{elemento.texto}</a></li>
+        {links.map((elemento, index) => 
+        <li key={index} className="nav-principal__item">
+          <a href={elemento.url}>{elemento.texto}</a>
+        </li>
         )}
       </ul>
     </nav>
@@ -40,11 +46,9 @@ const Header = () => {
     {url: "#", texto: "Contacto"},
     {url: "#", texto: "FAQ"}
   ])
-
   
   const handleClick = () => {
     setLinks([...links, {url: "#", texto: "Nuevo"}]) // agrego un nuevo objeto al array
-
     console.log(links)
   }
 
@@ -53,9 +57,21 @@ const Header = () => {
       <h1>Header</h1>
       <Navbar dark={true} links={links}/>      
       <p>{links.length}</p>
+      { links.length > 6 && 
+        <Mensaje show={links.length === 7}>
+          Hay muchos mensajes
+        </Mensaje>
+      }
       <button onClick={handleClick}>Agregar</button>
     </header>
   )
+}
+
+function Mensaje({show=false, children}) {
+  if(show){
+    return <p>{children}</p>
+  }
+  return null;
 }
 
 const TituloSecundario = ({children}) => {
@@ -63,6 +79,19 @@ const TituloSecundario = ({children}) => {
     <h2>{children}</h2>
   )
 }
+
+// Render - Monta el componente en el DOM
+
+// cambia el estado
+// cambia el contexto (hooks)
+// cambia las props
+
+// function Mensaje({show=false, children}) {
+//  if(show){
+//    return <p>{children}</p>
+//  }
+
+
 
 function App()
 {
@@ -77,6 +106,7 @@ function App()
         <span>Título 2</span>
       </TituloSecundario>
       
+     
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat tempora beatae modi, iste repellendus ad aliquid minus necessitatibus nesciunt accusamus esse harum est, veniam recusandae alias dolores magnam error!</p>
     </div>
   )
